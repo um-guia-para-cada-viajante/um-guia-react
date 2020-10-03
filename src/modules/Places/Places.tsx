5a24ba2d64d2666a372f5e78dcbc2ba126e1adfb
odilonjk <kroger.odilonj@gmail.com>
2020-10-03 18:57:02 -0300

    adicionados botoes na lista e scroll

--- src/modules/Places/Places.tsx
+++ src/modules/Places/Places.tsx
@@ -9,67 +9,66 @@ class Places extends React.Component {
   render() {
     const places = [
       {
-          "name": "Museu da Cerveja", 
+          "name": "Museu da Cervejaaaaaaaaaa aaaaaaaaaaa", 
           "image": "https://picsum.photos/200",
-          "rating": "5"
+          "rating": "5",
+          "description": "Croissant fruitcake jujubes cupcake chocolate danish. Powder sesame snaps cotton candy biscuit pudding pastry."
       },
       {
           "name": "Sorvete Schmitt", 
           "image": "https://picsum.photos/200",
-          "rating": "5"
+          "rating": "5",
+          "description": "Ice cream marzipan tootsie roll cheesecake. Brownie wafer liquorice sweet roll tiramisu tart chupa chups bonbon."
       },
       {
           "name": "Museu da Água", 
           "image": "https://picsum.photos/200",
-          "rating": "4"
+          "rating": "4",
+          "description": "Cake donut jelly-o cotton candy tart fruitcake. Sweet marzipan marshmallow donut lollipop cake sugar plum. "
       },
       {
           "name": "Museu Hering", 
           "image": "https://picsum.photos/200",
-          "rating": "4"
+          "rating": "4",
+          "description": "Jelly-o cheesecake pastry dragée sesame snaps liquorice. Halvah macaroon marzipan."
       },
       {
           "name": "Galinha Kazulke", 
           "image": "https://picsum.photos/200",
-          "rating": "3"
+          "rating": "3",
+          "description": "Chocolate caramels ice cream jelly."
       },
       {
           "name": "Prainha", 
           "image": "https://picsum.photos/200",
-          "rating": "3"
+          "rating": "3",
+          "description": "Fruitcake tootsie roll dragée chocolate cake jelly beans pastry."
       }
     ];
     return (
-      <div className="container-fluid">
-        <h2>Monte seu roteiro</h2>  
-        <div className="container-fluid">
-          {places.map(p => 
-            <>
-              <div className="row p-2" key={p.name}>
-                <div className="place row">
-                  <div className="align-middle">
-                    <input type="checkbox"></input>
-                  </div>  
-                  <div>
-                    <img className="place-img rounded" alt="Imagem do local" src={p.image}/>                        
-                  </div>
-
-                  <div className="place-details">
-                    <span className="place-name">{p.name}</span>
-                    <img className="place-rating row w-25" alt="Avaliação" src={process.env.PUBLIC_URL + `/images/rating/${p.rating}.png`}/>
+      <div className="container-fluid" style={{ overflowY: 'scroll', height: 'calc(100vh - 127px)' }}>
+        <h2 className="text-center p-2">Monte seu roteiro</h2>  
+        {places.map(p => 
+          <>
+            <div className="container-fluid">
+                <div className="row">
+                  <img className="place-img rounded" alt={`Imagem do local ${p.name}`} src={p.image}/>                        
+                  <div className="col-md">
+                    <h4 className="row">{p.name}</h4>
+                    <img className="row w-25" alt="Avaliação" src={process.env.PUBLIC_URL + `/images/rating/${p.rating}.png`}/>
                   </div>
                 </div>
-                <div className="row">
-                    <p>
-                      ow-income nobodies totally outrageous paradigm menapplause bagzooka homerhol
-                      homerhol paradigm no ones pinching his legs not back, avenge deaths doobob 
-                    </p>
-                  </div>
-              </div>
-              <hr/>
-            </>)
-          }
-        </div>
+                <div className="row p-1 mt-2">
+                  <p>{p.description}</p>
+                </div>
+                <div className="row justify-content-around">
+                  <button className="btn btn-primary">Selecionar</button>
+                  <button className="btn btn-outline-secondary center">Saiba mais</button>
+                </div>
+            </div>
+            <hr/>
+          </>)
+        }
       </div>
     )
   }
